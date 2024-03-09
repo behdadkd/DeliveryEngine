6e5053858f1514e385221d64266866b6ac3bf937
be.khodadadi <be.khodadadi@BE-KHODADADI>
2024-03-09 11:56:51 +0330

    .

--- Engine2/Program.cs
+++ Engine2/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Create RabbitMQ connection
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Text;
 
@@ -8,34 +9,49 @@ var timer = new Stopwatch();
 timer.Start();
 await Console.Out.WriteLineAsync(("Started => Time : " + timer.ElapsedMilliseconds));
 
+BindingList<Task> messageTasks = new();
+
 var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+factory.DispatchConsumersAsync = true;
+
 
 
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
 channel.QueueDeclare(queue: "requests", durable: false, exclusive: false, autoDelete: false, arguments: null);
-var hitCount = 0;
 
 using (HttpClient httpClient = new())
 {
-
-    var consumer = new EventingBasicConsumer(channel);
+    var consumer = new AsyncEventingBasicConsumer(channel);
 
     consumer.Received += async (model, ea) =>
     {
         var body = ea.Body.ToArray();
         var message = Encoding.UTF8.GetString(body);
-        //Console.WriteLine(" [x] Received {0}", message);
 
-        var response = await httpClient.PostAsync($"http://localhost:12345/delayMessage?message={message}", null);
-        hitCount++;
+        messageTasks.Add(MakeRequestAsync(httpClient, message));
+        channel.BasicAck(ea.DeliveryTag, false);
+
+        //await Console.Out.WriteLineAsync(("Hit => Time : " + timer.ElapsedMilliseconds));
     };
-    channel.BasicConsume(queue: "requests", autoAck: true, consumer: consumer);
 
+    messageTasks.ListChanged += async (sender, e) =>
+    {
+        if (messageTasks.Count == 5)
+        {
+            await Task.WhenAll(messageTasks);
+            messageTasks.Clear();
+            Console.WriteLine("5 EXE");
+        }
+    };
 
-    await Console.Out.WriteLineAsync(("End => Time : " + timer.ElapsedMilliseconds) + $"Hit : {hitCount}");
-
+    channel.BasicConsume(queue: "requests", autoAck: false, consumer: consumer);
     Console.WriteLine(" Press [enter] to exit.");
     Console.ReadLine();
+
+    static async Task MakeRequestAsync(HttpClient client, string message)
+    {
+        await client.PostAsync($"http://localhost:12345/delayMessage?message={message}", null);
+    }
 }
